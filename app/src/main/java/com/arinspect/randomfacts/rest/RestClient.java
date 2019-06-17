e3b6265346d59bf7a5f1edfab7a85098ffd4b3dd
Sagar Khakhrani <sagar.khakhrani@spec-india.com>
2019-06-17 20:34:43 +0530

    Implemented Facts Service in Rest Client

--- app/src/main/java/com/arinspect/randomfacts/rest/RestClient.java
+++ app/src/main/java/com/arinspect/randomfacts/rest/RestClient.java
@@ -1,15 +1,22 @@
 package com.arinspect.randomfacts.rest;
 
+import com.arinspect.randomfacts.rest.services.FactsService;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RestClient {
     private static final String BASE_URL = "https://dl.dropboxusercontent.com/";
+    private FactsService factsService;
     public RestClient() {
         Retrofit retrofit= new Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
                 .baseUrl(BASE_URL)
                 .build();
+        factsService= retrofit.create(FactsService.class);
     }
 
+    public FactsService getFactsService() {
+        return factsService;
+    }
 }
