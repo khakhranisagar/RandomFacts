f997a33a144f6ef4a9c38eff2ac430fd8db26b3d
Sagar Khakhrani <sagar.khakhrani@spec-india.com>
2019-06-18 17:52:05 +0530

    Implemented API call with the main View

--- app/src/main/java/com/arinspect/randomfacts/activities/MainActivity.java
+++ app/src/main/java/com/arinspect/randomfacts/activities/MainActivity.java
@@ -1,16 +1,149 @@
 package com.arinspect.randomfacts.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.arinspect.randomfacts.R;
+import com.arinspect.randomfacts.adapters.FactsAdapter;
+import com.arinspect.randomfacts.entities.CountryFact;
+import com.arinspect.randomfacts.entities.Fact;
+import com.arinspect.randomfacts.rest.RestClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private FactsAdapter factsAdapter;
+    private List<Fact> allFacts;
+    private List<Fact> recyclerItems;
+    private RestClient restClient;
+    private boolean isScrolling = false;
+    private ProgressBar progressBar;
+    private final String TAG="Error :"+ this.getClass().getCanonicalName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeInstanceComponents();
+        fetchData();
     }
+
+    private void fetchData() {
+        Call<CountryFact> countryFactCall = restClient.getFactsService().getCountryFacts();
+        countryFactCall.enqueue(countryFactCallback);
+    }
+
+    /**
+     * Initialization of initial Views and instance variables
+     */
+    private void initializeInstanceComponents(){
+        recyclerView = findViewById(R.id.rvFacts);
+        progressBar  = findViewById(R.id.pbFactList);
+        layoutManager = new LinearLayoutManager(this);
+        restClient= new RestClient();
+        recyclerView.setOnScrollListener(onScrollListener);
+    }
+
+    /**
+     * callback used to handle web api response
+     */
+    private Callback<CountryFact> countryFactCallback = new Callback<CountryFact>() {
+        @Override
+        public void onResponse(Call<CountryFact> call, Response<CountryFact> response) {
+            setAllFacts(response.body().getFacts());
+            recyclerItems = new ArrayList<>();
+            recyclerItems.addAll(allFacts.subList(0, 10));
+            factsAdapter = new FactsAdapter(recyclerItems, getApplicationContext());
+            recyclerView.setAdapter(factsAdapter);
+            recyclerView.setLayoutManager(layoutManager);
+            getSupportActionBar().setTitle(response.body().getTitle());
+        }
+
+        @Override
+        public void onFailure(Call<CountryFact> call, Throwable t) {
+
+        }
+    };
+
+    /**
+     * shows the progressbar
+     * Used to show progressBar when data is being fetched
+     */
+    private void showProgressBar(){
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * hides the progressbar
+     * Used to hide progressBar when data is fetched
+     */
+    private void hideProgressBar(){
+        progressBar.setVisibility(View.GONE);
+    }
+
+
+    /**
+     * onScrollListener used to know if user has scrolled to end, and if yes,
+     * next data will be loaded
+     */
+    private RecyclerView.OnScrollListener onScrollListener = new RecyclerView.OnScrollListener() {
+        @Override
+        public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled(recyclerView, dx, dy);
+            if (isScrolling && recyclerItems.size()< allFacts.size()) {
+                showProgressBar();
+                isScrolling = false;
+                loadMoreData();
+            }
+        }
+
+        @Override
+        public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+            super.onScrollStateChanged(recyclerView, newState);
+            if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                isScrolling = true;
+            }
+        }
+    };
+
+    /**
+     * Mocks the scenario of paging i.e Lazy loads more items in the list
+     */
+    private void loadMoreData() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                recyclerItems.addAll(allFacts.subList(7, allFacts.size()));
+                factsAdapter.notifyDataSetChanged();
+                hideProgressBar();
+            }
+        }, 3000);
+    }
+
+    /**
+     * Populates an instance of arraylist used in recycler
+     * @param allFacts
+     */
+    public void setAllFacts(List<Fact> allFacts) {
+        this.allFacts = allFacts;
+    }
+
 }
